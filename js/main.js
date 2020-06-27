669f880fee1feb31c76df31efe7f2cd095eb8ec8
Amanda Xavier Mariano <42248042+amandaxm@users.noreply.github.com>
2020-06-27 16:45:05 -0300

    Aula 1 introdução

---
+++ js/main.js
@@ -0,0 +1,58 @@
+
+
+
+
+/*
+var nome ="Amanda";
+var idade ="20";
+
+alert("Nome "+nome+", idade "+idade);
+console.log(nome.toLowerCase());
+console.log(nome.toUpperCase());
+console.log(idade);
+
+
+var lista =["maça", "couve", "laranja"];
+lista.push("uva");//insere
+//lista.pop();
+console.log(lista[0]);
+console.log(lista.length);//tamanho
+console.log(lista.reverse());//imprime lista a contrario
+console.log(lista.toString());//traz a lista string
+console.log(lista.join(" - "));//entre as posicoes
+
+**/
+/**
+ //dicionario
+var fruta ={nome: "maca", cor:"red"};
+console.log(fruta);
+console.log(fruta.nome);
+//lista de dicionario
+var frutas =[{nome: "maca", cor:"red"},{nome: "uva", cor:"roxa"}];
+console.log(frutas);
+console.log(frutas[1].nome);
+ */
+/**
+ var idade = prompt("Qual sua idade?");
+ if(idade>=18){
+     alert("maior de idade");
+ }
+ else{
+     alert("menor de idade");
+ }
+ */
+/**
+ * 
+ 
+ var count =0;
+ while(count <5){
+    console.log(count);
+    count = count+1;
+ }
+ */
+
+ var d =new Date();
+ alert(d);
+ alert(d.getMonth()+1);
+ alert(d.getMinutes());
+ 
